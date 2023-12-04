cd3396ad182afbe9dc0f4e5758bc01f0546f3cf1
Ben Laplanche <ben@laplanche.co.uk>
2023-12-04 17:24:03 +0000

    setting up cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,23 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aoc2023",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Advent of code 2023!",
+	Long:  `Advent of code 2023! Provide the day number as an argument, e.g. $ aoc-2023 -d 1`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) { fmt.Println("Hello world!") },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
